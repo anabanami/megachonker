30b1f58ada625c8fb19a650008998621984fee08
anabanami <ana.fabelah@gmail.com>
2020-03-13 15:16:04 +1100

    Add License. chonker is  now installable as python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chonker-anabanami", # Replace with your own username
+    version="2020.3",
+    author="Ana Fabela",
+    author_email="ana.fabelah@gmail.com",
+    description="Use chonker for optimizing the backup of large files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anabanami/chonker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
