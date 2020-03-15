b72bcb4e8f0a83f0efe27557406db91c676ffff6
anabanami <ana.fabelah@gmail.com>
2020-03-15 22:09:21 +1100

    update version number 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="chonker",
-    version="0.1",
+    version="0.2",
     author="Ana Fabela",
     author_email="ana.fabelah@gmail.com",
     description="Use chonker for optimizing the backup of large files.",
